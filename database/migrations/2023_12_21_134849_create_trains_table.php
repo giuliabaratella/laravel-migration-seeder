08211abf315d58ace26d15f20c7370fda8f50149
giuliabaratella <giuliabaratella7@gmail.com>
2023-12-21 15:12:41 +0100

    added migration + table trains + fields

---
+++ database/migrations/2023_12_21_134849_create_trains_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('company', 255);
+            $table->string('departure_station', 255);
+            $table->string('arrival_station', 255);
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->string('train_code', 8)->unique();
+            $table->tinyInteger('n_carriages')->unisgned();
+            $table->boolean('in_time')->default(1);
+            $table->boolean('deleted')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
